ff124c7384a513fac1491004c89306d252474167
alexeykagansky <alexey.kagansky@cloudinary.com>
2020-12-23 17:56:02 +0200

    init

--- packages/test/wdio.conf.js
+++ packages/test/wdio.conf.js
@@ -22,20 +22,15 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
-
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+    capabilities: [
+        {
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                args: ['--window-size=1920,1080', '--incognito', '-–ignore-certificate-errors', '--headless'],
+            },
+        },
+    ],
     //
     // ===================
     // Test Configurations
